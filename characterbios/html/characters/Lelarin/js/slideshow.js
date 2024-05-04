d33cda6f0770d63a7c5778ffd9dd8f35f14dcb2f
youngmichaelrepo <youngermichael@yahoo.com>
2024-05-04 16:15:19 -0500

    Update slideshow.js

--- characterbios/html/characters/Lelarin/js/slideshow.js
+++ characterbios/html/characters/Lelarin/js/slideshow.js
@@ -10,5 +10,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 3 seconds
+  setTimeout(carousel, 5000); // Change image every 5 seconds
 }
